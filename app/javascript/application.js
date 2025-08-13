c080cbcd98e71241e73c5ef769a00ca1d3d5325e
g1ftmkyelu <mkyelugift@gmail.com>
2025-08-13 11:49:56 +0200

    Edit: Project edited: the toogles, menus, modals etc are not interactive they are anot opening and toggling avery interact (2025-08-13T09:49:56.058Z)

---
+++ app/javascript/application.js
@@ -0,0 +1,3 @@
+// Configure your import map in config/importmap.rb. Read more: https://guides.rubyonrails.org/hotwire.html#importmap
+import "@hotwired/turbo-rails"
+import "controllers" // This line imports all Stimulus controllers from app/javascript/controllers
